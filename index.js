a09e0fe7b8f788757fa91b072569a019d4c51040
Daniel Fernandez <danny.fdez99@gmail.com>
2021-03-15 23:13:33 -0400

    Added the other endpoints for the api. Created a code.js file that will handle functioanlity in the frontend.

--- index.js
+++ index.js
@@ -58,5 +58,37 @@ app.get('/api/teams', (req, res) => {
 	res.json(db.map(team => team.name));
 });
 
+
+app.get('/api/team/:teamName', (req, res) => {
+	if(db.some(team => team.name === req.params.teamName)){
+		let team = db.filter(team => team.name === req.params.teamName)[0];
+		res.json({name: team.name, created: team.created, players: team.players.map(player => player.name)});
+	}
+	else{
+		res.status(400).json({msg: `${req.params.teamName} is not a team we have registered`});
+	}
+});
+
+
+app.get('/api/player/:playerName', (req, res) => {
+	let found = false;
+	let playerTeam = undefined;
+
+	for(let team of db){
+		if(team.players.map(player => player.name).includes(req.params.playerName)){
+			found = true;
+			playerTeam = team;
+		}
+	}
+	if(found){
+		let player = playerTeam.players.filter(player => player.name === req.params.playerName)[0];
+		res.json(player);
+	}
+	else{
+		res.status(400).json({msg: `${req.params.playerName} is not a team we have registered`});
+	}
+});
+
+
 const PORT = process.env.PORT || 5000; // Setting the PORT number
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); // Making the app listen on PORT (and localhost by default)
---
+++ public/code.js
@@ -0,0 +1,13 @@
+const allTeamsBtn = document.querySelector('#all-teams-actionable');
+
+
+const teamBtn = document.querySelector('#team-actionable');
+const teamEntryGroup = document.querySelector('#team-entry-group');
+const teamInput = document.querySelector('#team-input');
+const teamSubmit = document.querySelector('#team-find');
+
+
+const playerBtn = document.querySelector('#player-actionable');
+const playerEntryGroup = document.querySelector('#player-entry-group');
+const playerInput = document.querySelector('#player-input');
+const playerSubmit = document.querySelector('#player-find');
